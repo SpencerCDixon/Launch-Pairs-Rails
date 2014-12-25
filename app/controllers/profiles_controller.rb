0d855650670f2d0a54d3854ae3b225c5bb7e70d1
Spencer Dixon <spencercdixon@gmail.com>
2014-12-24 19:17:44 -0500

    Finished profile feature for users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_filter :authenticate, only: :show
+
   def show
     @user = User.find(params[:user_id])
   end
--- spec/features/user_can_visit_profile_spec.rb
+++ spec/features/user_can_visit_profile_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 feature 'User can visit their profile' do
+  let(:user) { FactoryGirl.create(:user) }
+
   scenario 'auth user sees welcome message' do
-    user = FactoryGirl.create(:user)
     sign_in_as(user)
     visit user_profile_path(user)
 
-    expect(page).to have_content("Welcome, #{user.first_name}")
+    expect(page).to have_content("#{user.first_name}'s Profile")
   end
 
   scenario 'unauth user sees the users name' do
-    user = FactoryGirl.create(:user)
+    visit user_profile_path(user)
+
+    expect(page).to have_content("Launch Pairs")
   end
 end
