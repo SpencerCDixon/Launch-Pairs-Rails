48a1e496e80e156cd8041a740be71a0ccf5a10f1
Spencer Dixon <spencercdixon@gmail.com>
2014-12-27 12:35:04 -0500

    Added a has_one relationship to my user model to show how to create nice helper association methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ActiveRecord::Base
   has_many :statuses
   has_many :projects
 
+  # Testing adding a custom has_on association:
+  has_one :latest_status, -> { order('created_at desc') }, class_name: 'Status'
+  # This works but breaks my views b/c they call on nil.  Need a way to create an empty status everytime a user gets created.
+
+  
+
   def current_status
     if !statuses.empty?
       statuses.last.description 
