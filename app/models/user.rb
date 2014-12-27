e5b64218bb3cd62e5d250e58d38fd8891eceb384
Spencer Dixon <spencercdixon@gmail.com>
2014-12-27 18:07:30 -0500

    Updated some comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,8 @@ class User < ActiveRecord::Base
   has_many :projects
 
   # Testing adding a custom has_on association:
-  has_one :latest_status, -> { order('created_at desc') }, class_name: 'Status'
+  has_one :latest_status, -> { order('created_at desc') }, class_name: 'Status', dependent: :destroy
   # This works but breaks my views b/c they call on nil.  Need a way to create an empty status everytime a user gets created.
-
-  
 
   def current_status
     if !statuses.empty?
