13136fea84543ce06c0caccb73a0211050702713
Spencer Dixon <spencercdixon@gmail.com>
2014-12-27 11:23:19 -0500

    Finished model tests for status to only be the 3 possible options

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,7 @@
 class Status < ActiveRecord::Base
   belongs_to :user
+
+  validates :description, presence: true, inclusion: { in: ['Open To Help',
+                                                            'Ready To Pair',
+                                                            'In The Zone'] }
 end
