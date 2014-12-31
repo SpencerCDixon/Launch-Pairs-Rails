363e44020fd1755d6e40cf6110e34e22fd487cb1
Spencer Dixon <spencercdixon@gmail.com>
2014-12-30 19:55:59 -0500

    Wrote failing unit tests for pairings model

--- spec/models/pairing_spec.rb
+++ spec/models/pairing_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Pairing, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:user_id).when('1', '100') }
+  it { should_not have_valid(:user_id).when('', nil, 'sadflkjsdlfk') }
+
+  it { should have_valid(:pair_id).when('1', '100') }
+  it { should_not have_valid(:pair_id).when('', nil, 'sadflkjsdlfk') }
 end
