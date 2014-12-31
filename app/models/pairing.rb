c2f1ce0b40d0afbd079307306d7b324cef391c8e
Spencer Dixon <spencercdixon@gmail.com>
2014-12-30 19:56:44 -0500

    Passing unit tests for pairings

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -1,4 +1,7 @@
 class Pairing < ActiveRecord::Base
   validates_uniqueness_of :user_id, scope: [:pair_id]
+  validates :user_id, presence: true, numericality: true
+  validates :pair_id, presence: true, numericality: true
+
   belongs_to :user
 end
