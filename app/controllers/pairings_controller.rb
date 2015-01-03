c5b6f99dd1f74c44569a30e2a76aa296f68b6ea9
Spencer Dixon <spencercdixon@gmail.com>
2015-01-03 11:20:44 -0500

    Refactorings

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -4,8 +4,8 @@ class PairingsController < ApplicationController
 
     if @pairing.save
       redirect_to dashboard_path, notice: "Pairing complete!"
-    else
-      redirect_to dashboard_path, alert: "Already paired with that person!"
+    # else
+      # redirect_to dashboard_path, alert: "Already paired with that person!"
     end
   end
 end
