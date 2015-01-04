454d8640e868cf55691539e714aaf8bd6589aa02
Spencer Dixon <spencercdixon@gmail.com>
2015-01-03 19:28:38 -0500

    Added unit tests for validations on the profiles model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,12 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id, presence: true, numericality: true
+  validates :facebook, format: /\A(http|https)/
+  validates :twitter, format: /\A(http|https)/
+  validates :github, format: /\A(http|https)/
+  validates :linked_in, format: /\A(http|https)/
+  validates :blog_url, format: /\A(http|https)/
+  validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
+
 end
