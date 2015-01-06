eb2ec0c1c965f86d09ba74f08a5eb3d6974f61a8
Spencer Dixon <spencercdixon@gmail.com>
2015-01-06 18:11:07 -0500

    Refactored model test for StudentRegistration and some Dashboard tests

--- spec/features/status_spec.rb
+++ spec/features/status_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 feature "status" do
 
   context "authorized user" do
-    let(:user) { FactoryGirl.create(:user) }
+    let(:complete_user) { FactoryGirl.create(:complete_user) }
 
     before(:each) do
-      sign_in_as(user)
+      sign_in_as(complete_user)
+    end
+
+    scenario "default status is Sleeping" do
+      visit user_profile_path(complete_user)
+      expect(page).to have_content("Sleeping")
     end
 
     scenario "creates status successfully" do
-      visit user_profile_path(user)
-
+      visit user_profile_path(complete_user)
       update_status("In The Zone")
 
       expect(page).to_not have_content("Sleeping")
@@ -19,14 +23,12 @@ feature "status" do
     end
 
     scenario "status gets updated on dashboard" do
-      visit user_profile_path(user)
-
+      visit user_profile_path(complete_user)
       update_status("In The Zone")
 
       visit dashboard_path
       expect(page).to have_content("In The Zone")
     end
-
   end
 end
 
--- spec/models/student_registration_spec.rb
+++ spec/models/student_registration_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 describe StudentRegistration do
   context 'when registering' do
+    let(:user) { FactoryGirl.create(:user) }
 
-    it 'if the save is successful it creates empty project' do
-      user = FactoryGirl.create(:user)
+    before(:each) do
       registration = StudentRegistration.new(user)
       registration.create_dependencies
+    end
 
-      expect(user.statuses.count).to eq(1)
+    it 'if the save is successful it creates empty project' do
       expect(user.projects.count).to eq(1)
     end
-    it 'if the save is successful it creates an empty status'
 
+    it 'if the save is successful it creates an empty status' do
+      expect(user.statuses.count).to eq(1)
+    end
   end
 end
