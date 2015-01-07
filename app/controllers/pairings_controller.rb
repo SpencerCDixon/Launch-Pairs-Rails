92d154b66e2bc748aad49f0b728ee328e726fdf9
Spencer Dixon <spencercdixon@gmail.com>
2015-01-07 10:59:28 -0500

    Changed where I put the files for ajax to see if it would work, instead broke stuff

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -1,9 +1,14 @@
 class PairingsController < ApplicationController
+  respond_to :js, :html
+
   def create
     @pairing = Pairing.new(user_id: current_user.id, pair_id: params[:pair_id])
 
     if @pairing.save
-      redirect_to dashboard_path, info: "Pairing complete!"
+      respond_to do |format|
+        format.html { redirect_to dashboard_path, info: "Pairing complete!" }
+        format.js
+      end
     else
       redirect_to root_path
     end
